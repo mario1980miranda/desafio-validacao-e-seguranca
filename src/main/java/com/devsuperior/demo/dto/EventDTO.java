cc379e8e414ad25cc088cac1ea731902747038d6
Mario Miranda <mario_chan15@hotmail.com>
2023-07-26 13:44:22 -0400

    Test OK - insertShouldReturn422WhenAdminLoggedAndNullCity

--- src/main/java/com/devsuperior/demo/dto/EventDTO.java
+++ src/main/java/com/devsuperior/demo/dto/EventDTO.java
@@ -4,12 +4,17 @@ import java.time.LocalDate;
 
 import com.devsuperior.demo.entities.Event;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class EventDTO {
 	
 	private Long id;
+	@NotBlank(message = "Campo requerido")
 	private String name;
 	private LocalDate date;
 	private String url;
+	@NotNull(message = "Campo requerido")
 	private Long cityId;
 	
 	public EventDTO() {
